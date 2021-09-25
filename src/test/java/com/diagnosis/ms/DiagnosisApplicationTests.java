7e3b8b4c0d7c60131043eba004f60859a22a1c00
Sayali Nerkar <Sayali.Nerkar@citiustech.com>
2021-09-25 18:44:32 +0530

    initial diagnosis repo

---
+++ src/test/java/com/diagnosis/ms/DiagnosisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diagnosis.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiagnosisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
